63ca1dc09b2bd77a7418669c311a9c10bcb34942
IRonny1 <ironny1604@gmail.com>
2026-05-11 23:07:05 +0300

    feat(app): register DiaryModule, WaterModule, WeightModule in AppModule

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -6,9 +6,21 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { ProfileModule } from './profile/profile.module';
 import { ScanModule } from './scan/scan.module';
+import { DiaryModule } from './diary/diary.module';
+import { WaterModule } from './water/water.module';
+import { WeightModule } from './weight/weight.module';
 
 @Module({
-  imports: [ConfigModule, PrismaModule, AuthModule, ProfileModule, ScanModule],
+  imports: [
+    ConfigModule,
+    PrismaModule,
+    AuthModule,
+    ProfileModule,
+    ScanModule,
+    DiaryModule,
+    WaterModule,
+    WeightModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
